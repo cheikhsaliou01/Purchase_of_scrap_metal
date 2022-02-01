9f99cff86569963986fafa024b84030835989319
CoderYooda <CoderYooda@gmail.com>
2022-02-01 11:27:00 +0300

    awd

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,12 @@
 
+window.carousel = new Carousel({
+  "main": ".js-carousel",
+  "wrap": ".js-carousel__wrap",
+  "prev": ".js-carousel__prev",
+  "next": ".js-carousel__next"
+});
+
+console.log(carousel);
 //Active menu burger 
 window.toggleMenu = () => {
   let x = document.getElementsByClassName('header__burger')[0];
@@ -49,6 +57,7 @@ let btn = document.getElementById('mybtn');
 let span = document.getElementsByClassName('close')[0];
 let backr = document.getElementsByClassName('model__background');
 let modelContent = document.getElementsByClassName('model__container');
+
 btn.onclick = function () {
   modal.style.display = "block";
 }
@@ -70,4 +79,63 @@ map = new google.maps.Map(document.getElementById('map'), {
   zoom: 8
 });
 
+
+function Carousel(setting) {
+
+  /* Scope privates methods and properties */
+  let privates = {};
+
+  /* Privates properties */
+  privates.setting = setting;
+
+  privates.sel = {
+    "main": document.querySelector(privates.setting.main),
+    "wrap": document.querySelector(privates.setting.wrap),
+    "children": document.querySelector(privates.setting.wrap).children,
+    "prev": document.querySelector(privates.setting.prev),
+    "next": document.querySelector(privates.setting.next)
+  };
+
+  privates.opt = {
+    "position": 0,
+    "max_position": document.querySelector(privates.setting.wrap).children.length
+  };
+
+  // Control
+  if(privates.sel.prev !== null) {
+    privates.sel.prev.addEventListener('click', () => {
+      this.prev_slide();
+    });
+  }
+
+  if(privates.sel.next !== null) {
+    privates.sel.next.addEventListener('click', () => {
+      this.next_slide();
+    });
+  }
+
+  this.prev_slide = () => {
+    --privates.opt.position;
+
+    if(privates.opt.position < 0) {
+      privates.sel.wrap.classList.add('s-notransition');
+      privates.opt.position = privates.opt.max_position - 1;
+    }
+
+    privates.sel.wrap.style["transform"] = `translateX(-${privates.opt.position}00%)`;
+  };
+
+
+// Next slide
+  this.next_slide = () => {
+    ++privates.opt.position;
+
+    if(privates.opt.position >= privates.opt.max_position) {
+      privates.opt.position = 0;
+    }
+
+    privates.sel.wrap.style["transform"] = `translateX(-${privates.opt.position}00%)`;
+  };
+
+}
 
